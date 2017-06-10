e837585392ca9d1f9c210bb1e2fd29d60b4e5a48
Igor <joyukr>
2017-06-10 09:19:58 +0300

    Initial commit

---
+++ src/main/java/start/AppManager.java
@@ -0,0 +1,22 @@
+package start;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Igor Hnes on 6/10/17.
+ */
+public class AppManager extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+        primaryStage.setTitle("Itea");
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
